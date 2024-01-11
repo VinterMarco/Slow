310afbb4df5d1fcbb9ef59fbd9303d4d110f4ccf
Vinter Marco <vintermarco98@gmail.com>
2024-01-11 14:16:13 +0200

    test 11

--- Application/Slow/Slow/Model/User.swift
+++ Application/Slow/Slow/Model/User.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+
+
+import Foundation
+
+struct User : Identifiable, Codable {
+    let id : String
+    let fullName : String
+    let emai : String
+    
+    var initials : String {
+        let formatter = PersonNameComponentsFormatter()
+        if let components = formatter.personNameComponents(from: fullName) {
+            formatter.style = .abbreviated
+            return formatter.string(from: components)
+        }
+        return ""
+    }
+}
+
+
+extension User {
+    static var MOCK_USER = User(id: NSUUID().uuidString, fullName: "Vinter Marco", emai: "vintermarco98@gmail.com")
+}
