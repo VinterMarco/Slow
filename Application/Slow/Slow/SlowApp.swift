84926a95766cfbd60e41e95e28e99ee0429124c2
Vinter Marco <vintermarco98@gmail.com>
2024-01-11 14:04:48 +0200

    a

---
+++ Application/Slow/Slow/SlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlowApp.swift
+//  Slow
+//
+//  Created by Vinter Marco on 11.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
